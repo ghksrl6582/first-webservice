46dabfec8ae453a9dc73c3e9a9ddd1f491cfed70
ghksrl6582 <ghksrl6582@daum.net>
2021-11-27 14:31:09 +0900

    메인페이지에 게시글 목록

--- src/main/java/org/example/springboot/web/IndexController.java
+++ src/main/java/org/example/springboot/web/IndexController.java
@@ -1,13 +1,21 @@
 package org.example.springboot.web;
 
+import lombok.RequiredArgsConstructor;
+import org.example.springboot.service.posts.PostsService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+
         return "index";
     }
 
